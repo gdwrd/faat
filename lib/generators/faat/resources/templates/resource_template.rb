3ef9049d3525e2002d10e28aad738b6b51e25cd4
Nazarii Sheremet <xo8bit@gmail.com>
2016-01-05 15:40:11 +0200

    add faat:resources generators

---
+++ lib/generators/faat/resources/templates/resource_template.rb
@@ -0,0 +1,11 @@
+<%= '# default generated template' %>
+
+<%= "# for using with model resource, initialize #{resource_name}Resource" %>
+<%= "# #{resource_name}Resource.new( ModelClassName.new )" %>
+<%= "# or with instance variable #{resource_name}Resource.new(@model_class_name)" %>
+
+class <%= resource_name %>Resource < Faat::Resources::Base
+
+  # add your business logic here
+
+end
